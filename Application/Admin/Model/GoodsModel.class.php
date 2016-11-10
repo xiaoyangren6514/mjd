71b2ed9e042f1d12d28a13967521fc4a8a40101c
zhonglongquan <zhonglongquan@rong360.com>
2016-11-10 17:09:22 +0800

    增加图片上传及缩略图功能

--- Application/Admin/Model/GoodsModel.class.php
+++ Application/Admin/Model/GoodsModel.class.php
@@ -2,7 +2,9 @@
 
 namespace Admin\Model;
 
+use Think\Image;
 use Think\Model;
+use Think\Upload;
 
 /**
  * Created by PhpStorm.
@@ -39,6 +41,72 @@ class GoodsModel extends Model
     {
         $data['addtime'] = date('Y-m-d H:i:s', time());
         $data['goods_desc'] = removeXSS($_POST['goods_desc']);
+        // 处理logo
+        $fileErrorNo = $_FILES['logo']['error'];
+        if ($fileErrorNo == 0) {
+            // 初始化上传配置信息
+            $config = array(
+                'exts' => array('jpg', 'jpeg', 'png', 'bmp'), //允许上传的文件后缀
+                'rootPath' => './Public/Uploads/', //保存根路径
+                'savePath' => 'Goods/', //保存路径
+            );
+            $up = new Upload($config);
+            // uploadOne方法会返回上传附件存储在服务器的 名字 路径等信息
+            /**
+             * array (size=9)
+             * 'name' => string 'debug.log' (length=9)
+             * 'type' => string 'application/octet-stream' (length=24)
+             * 'size' => int 6123
+             * 'key' => int 0
+             * 'ext' => string 'log' (length=3)
+             * 'md5' => string '88c9f4aba7287131ede4c9f21d64683e' (length=32)
+             * 'sha1' => string '9369b5f5f40e68ba01146ac07819882c1dec98ad' (length=40)
+             * 'savename' => string '581b197e70fdf.log' (length=17)
+             * 'savepath' => string '2016-11-03/' (length=11)
+             */
+            $z = $up->uploadOne($_FILES['logo']);
+            if ($z) {// upload success
+                // 把上传好的图片保存到数据表中
+                //  ./Public/Uploads/2016-11-03/581b19ec127ff.log
+                $logo = $up->rootPath . $z['savepath'] . $z['savename'];
+                $_POST['logo'] = ltrim($logo, './');
+                // 给上传好的图片制作缩略图  130 350 50 700
+                $im = new Image();// 实例化image对象
+                $im->open($logo);// 打开目标文件
+
+                $smPathName = $up->rootPath . $z['savepath'] . "sm_" . $z['savename'];
+                $midlPathName = $up->rootPath . $z['savepath'] . "mid_" . $z['savename'];
+                $bigPathName = $up->rootPath . $z['savepath'] . "big_" . $z['savename'];
+                $mbigPathName = $up->rootPath . $z['savepath'] . "mbig_" . $z['savename'];
+                $im->thumb(700, 700, 6)->save($mbigPathName);// 保存图片到服务器
+                $im->thumb(350, 350, 6)->save($bigPathName);// 保存图片到服务器
+                $im->thumb(130, 130, 6)->save($midlPathName);// 保存图片到服务器
+                $im->thumb(50, 50, 6)->save($smPathName);// 制作缩略图 默认有自适应效果  保存图片到服务器
+
+                $data['logo'] = $logo;
+                $data['sm_logo'] = $smPathName;
+                $data['mid_logo'] = $midlPathName;
+                $data['big_logo'] = $bigPathName;
+                $data['mbig_logo'] = $mbigPathName;
+            } else {
+                $this->error = $up->getError();
+                return false;
+            }
+        } else if ($fileErrorNo == 1) {
+            $this->error = '上传的文件超过了 php.ini 中 upload_max_filesize 选项限制的值';
+            return false;
+        } else if ($fileErrorNo == 2) {
+            $this->error = '上传文件的大小超过了 HTML 表单中 MAX_FILE_SIZE 选项指定的值';
+            return false;
+        } else if ($fileErrorNo == 3) {
+            $this->error = '文件只有部分被上传';
+            return false;
+        } /*else if ($fileErrorNo == 4) {
+            $this->redirect('add', array(), 1, '没有文件被上传');
+        } */ else if ($fileErrorNo == 5) {
+            $this->error = '上传文件大小为0';
+            return false;
+        }
     }
 
 
